8d43ee6a78d2183460c204e7d1388498d0e53c11
RajuYuva <nagarajugoud12@gmail.com>
2025-02-24 22:51:57 +0530

    more scripts

--- process.sh
+++ process.sh
@@ -25,6 +25,8 @@
 
 #### multiple process to check
 
+# #!/bin/bash
+
 # for i in "$@"
 # do
 #     process_check=$(pgrep -x "$i")
@@ -37,13 +39,14 @@
 # done
 
 ## another way of multiple process to check
-for i in "$@"; do
-    if pgrep -x "$i" > /dev/null; then
-        echo "$i is running"
-    else
-        echo "$i is not running"
-    fi
-done        
+# #!/bin/bash
+# for i in "$@"; do
+#     if pgrep -x "$i" > /dev/null; then
+#         echo "$i is running"
+#     else
+#         echo "$i is not running"
+#     fi
+# done        
 
 ### output will be like below
 
@@ -55,3 +58,18 @@ done
 # terraform is not running
 # nginx is running
 # nginxx is not running
+
+## another way thru reading input using read -p
+#!/bin/bash
+
+# Prompt the user to enter process names separated by spaces
+read -p "Enter process names separated by spaces: " -a processes
+
+# Loop through each process name
+for i in "${processes[@]}"; do
+    if pgrep -x "$i" > /dev/null; then
+        echo "$i is running"
+    else
+        echo "$i is not running"
+    fi
+done
