79ed1655bf28992b5b0d7f54022e0f07dea8189c
RajuYuva <nagarajugoud12@gmail.com>
2025-03-13 13:52:30 +0530

    readme

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,11 +1,20 @@
+# #!/bin/bash
+
+# echo "Please enter username::"
+
+# read -s USERNAME #here USERNAME is variable
+
+# echo "Please enter password::"
+
+# read -s PASSWORD
+
+# echo "Username is: $USERNAME, Password is: $PASSWORD"
+
 #!/bin/bash
 
-echo "Please enter username::"
+read -p "Please enter username: " USERNAME
 
-read -s USERNAME #here USERNAME is variable
-
-echo "Please enter password::"
-
-read -s PASSWORD
+read -sp "Please enter password: " PASSWORD
+echo  # add a newline after the hidden password input
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
